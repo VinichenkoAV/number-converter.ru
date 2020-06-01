24e224db3b938705b36a5c74fb7f5568b248521e
VinichenkoAV <vinichenko.av.rus@gmail.com>
2020-06-01 10:46:26 +0300

    ver 2.0.11

---
+++ main-bigint.js
@@ -0,0 +1,334 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// install a JSONP callback for chunk loading
+/******/ 	function webpackJsonpCallback(data) {
+/******/ 		var chunkIds = data[0];
+/******/ 		var moreModules = data[1];
+/******/ 		var executeModules = data[2];
+/******/
+/******/ 		// add "moreModules" to the modules object,
+/******/ 		// then flag all "chunkIds" as loaded and fire callback
+/******/ 		var moduleId, chunkId, i = 0, resolves = [];
+/******/ 		for(;i < chunkIds.length; i++) {
+/******/ 			chunkId = chunkIds[i];
+/******/ 			if(Object.prototype.hasOwnProperty.call(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 				resolves.push(installedChunks[chunkId][0]);
+/******/ 			}
+/******/ 			installedChunks[chunkId] = 0;
+/******/ 		}
+/******/ 		for(moduleId in moreModules) {
+/******/ 			if(Object.prototype.hasOwnProperty.call(moreModules, moduleId)) {
+/******/ 				modules[moduleId] = moreModules[moduleId];
+/******/ 			}
+/******/ 		}
+/******/ 		if(parentJsonpFunction) parentJsonpFunction(data);
+/******/
+/******/ 		while(resolves.length) {
+/******/ 			resolves.shift()();
+/******/ 		}
+/******/
+/******/ 		// add entry modules from loaded chunk to deferred list
+/******/ 		deferredModules.push.apply(deferredModules, executeModules || []);
+/******/
+/******/ 		// run deferred modules when all chunks ready
+/******/ 		return checkDeferredModules();
+/******/ 	};
+/******/ 	function checkDeferredModules() {
+/******/ 		var result;
+/******/ 		for(var i = 0; i < deferredModules.length; i++) {
+/******/ 			var deferredModule = deferredModules[i];
+/******/ 			var fulfilled = true;
+/******/ 			for(var j = 1; j < deferredModule.length; j++) {
+/******/ 				var depId = deferredModule[j];
+/******/ 				if(installedChunks[depId] !== 0) fulfilled = false;
+/******/ 			}
+/******/ 			if(fulfilled) {
+/******/ 				deferredModules.splice(i--, 1);
+/******/ 				result = __webpack_require__(__webpack_require__.s = deferredModule[0]);
+/******/ 			}
+/******/ 		}
+/******/
+/******/ 		return result;
+/******/ 	}
+/******/
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// object to store loaded and loading chunks
+/******/ 	// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 	// Promise = chunk loading, 0 = chunk loaded
+/******/ 	var installedChunks = {
+/******/ 		"main-bigint": 0
+/******/ 	};
+/******/
+/******/ 	var deferredModules = [];
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
+/******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
+/******/ 	jsonpArray.push = webpackJsonpCallback;
+/******/ 	jsonpArray = jsonpArray.slice();
+/******/ 	for(var i = 0; i < jsonpArray.length; i++) webpackJsonpCallback(jsonpArray[i]);
+/******/ 	var parentJsonpFunction = oldJsonpFunction;
+/******/
+/******/
+/******/ 	// add entry module to deferred list
+/******/ 	deferredModules.push(["./scripts/form-bigint.js","main-bigint~main-sum"]);
+/******/ 	// run deferred modules when ready
+/******/ 	return checkDeferredModules();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./scripts/converter-bigint.js":
+/*!*************************************!*\
+  !*** ./scripts/converter-bigint.js ***!
+  \*************************************/
+/*! exports provided: converterBigInt */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "converterBigInt", function() { return converterBigInt; });
+/* harmony import */ var _typewriter_bigint_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./typewriter-bigint.js */ "./scripts/typewriter-bigint.js");
+
+
+
+function converterBigInt(user_value) {
+  // Фрагмент кода удаляет пробелы.
+  user_value = user_value.replace(/\s/g, function (x) {
+    return "";
+  });
+  document.getElementById("resultBigint").innerText = "2"; // Проверка на допустимость вводимого значения
+
+  if (!_typewriter_bigint_js__WEBPACK_IMPORTED_MODULE_0__["default"].checkUserBigInt(user_value)) {
+    return false;
+  }
+
+  document.getElementById("resultBigint").innerText = "-1"; // Основной код функции.
+
+  return "".concat(new _typewriter_bigint_js__WEBPACK_IMPORTED_MODULE_0__["default"](user_value));
+}
+
+/***/ }),
+
+/***/ "./scripts/form-bigint.js":
+/*!********************************!*\
+  !*** ./scripts/form-bigint.js ***!
+  \********************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _converter_bigint_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./converter-bigint.js */ "./scripts/converter-bigint.js");
+/* harmony import */ var _working_with_results_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./working-with-results.js */ "./scripts/working-with-results.js");
+
+
+
+ // Функция анимации вывода результата для счета.
+
+function printResult3() {
+  setTimeout(function () {
+    document.getElementById("resultBigint").innerText = resultBigNum.slice(0, y);
+
+    if (document.getElementById("resultBigint").innerText !== resultBigNum) {
+      y += 1;
+      window.requestAnimationFrame(printResult3);
+    }
+  }, 3);
+} // Блок кода с реализацией вывода результата большого числа.
+
+
+var form2 = document.getElementById("second-form");
+var resultBigNum;
+var y;
+form2.addEventListener("submit", function (event) {
+  event.preventDefault();
+  resultBigNum = Object(_converter_bigint_js__WEBPACK_IMPORTED_MODULE_0__["converterBigInt"])(form2.elements.user_value.value);
+
+  if (!resultBigNum) {
+    var _y = "Введите допустимое значение.";
+    document.getElementById("resultBigint").innerText = _y;
+  } else {
+    y = 0;
+
+    try {
+      window.sessionStorage.setItem("storage2", window.JSON.stringify([form2.elements.user_value.value, resultBigNum]));
+    } catch (err) {
+      Object(_working_with_results_js__WEBPACK_IMPORTED_MODULE_1__["catchUncaughtDOMException"])(err);
+    }
+
+    window.requestAnimationFrame(printResult3);
+  }
+}); // Реализация кнопки "Очистить" второй формы.
+
+form2.addEventListener("reset", function (event) {
+  event.preventDefault();
+  document.getElementById("second-form").elements.user_value.value = "";
+  resultBigNum = "";
+  document.getElementById("resultBigint").innerText = resultBigNum;
+
+  try {
+    window.sessionStorage.setItem("storage2", window.JSON.stringify([document.getElementById("second-form").elements.user_value.value, resultBigNum]));
+  } catch (err) {
+    Object(_working_with_results_js__WEBPACK_IMPORTED_MODULE_1__["catchUncaughtDOMException"])(err);
+  }
+});
+
+/***/ }),
+
+/***/ "./scripts/typewriter-bigint.js":
+/*!**************************************!*\
+  !*** ./scripts/typewriter-bigint.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return TypewriterBigint; });
+/* harmony import */ var _typewriter_sum_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./typewriter-sum.js */ "./scripts/typewriter-sum.js");
+
+
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+
+
+var TypewriterBigint = /*#__PURE__*/function (_TypewriterSum) {
+  _inherits(TypewriterBigint, _TypewriterSum);
+
+  var _super = _createSuper(TypewriterBigint);
+
+  _createClass(TypewriterBigint, null, [{
+    key: "checkUserBigInt",
+    value: function checkUserBigInt(user_value) {
+      document.getElementById("resultBigint").innerText = "10";
+
+      try {
+        BigInt(user_value);
+        return true;
+      } catch (err) {
+        if (err.name === "SyntaxError") {
+          return false;
+        } else throw err;
+      }
+    }
+  }]);
+
+  function TypewriterBigint(first) {
+    var _this;
+
+    _classCallCheck(this, TypewriterBigint);
+
+    _this = _super.call(this, "RUB", first);
+    document.getElementById("resultBigint").innerText = "5";
+    _this.first = BigInt(first).toString();
+    document.getElementById("resultBigint").innerText = "6";
+    return _this;
+  }
+
+  return TypewriterBigint;
+}(_typewriter_sum_js__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=main-bigint.js.map
